7b87ab918275a0782933ebd4a1ec09bc688a0482
UsenkoAnastasya <usenkonasty@gmail.com>
2026-03-01 21:19:18 +0200

    виправили помилкияяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяя3

--- lab1/code.js
+++ lab1/code.js
@@ -1,21 +1,20 @@
 function* FibonacciGenerator(){
-    let prev=0;
-    let curr=1;
+    let prev = 0;
+    let curr = 1;
 while(true){
     yield prev;
-const next=prev+curr;
-prev=curr;
-curr=next;
+const next = prev + curr;
+prev = curr;
+curr = next;
 }
 }
 function  TimeoutIterator(iterator,timeout){
     const startTime= Date.now();
 const timeoutMs = timeout*1000;
 for( const value of iterator){
-    if(Date.now-startTime >= timeoutMs) break;
+    if(Date.now()-startTime >= timeoutMs) break;
+    console.log(`${value}`);
 }
-console.log(`${value}`);
 }
-
 const fib = FibonacciGenerator();
 TimeoutIterator(fib, 5);
