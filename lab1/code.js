39b5bdb2556d2b8b0478fdc3649853eee3e275bc
UsenkoAnastasya <usenkonasty@gmail.com>
2026-03-01 21:20:46 +0200

    виправили помилки і додали пробіли

--- lab1/code.js
+++ lab1/code.js
@@ -9,10 +9,10 @@ curr = next;
 }
 }
 function  TimeoutIterator(iterator,timeout){
-    const startTime= Date.now();
+    const startTime = Date.now();
 const timeoutMs = timeout*1000;
 for( const value of iterator){
-    if(Date.now()-startTime >= timeoutMs) break;
+    if(Date.now() - startTime >= timeoutMs) break;
     console.log(`${value}`);
 }
 }
