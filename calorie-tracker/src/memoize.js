4e5511273a6daec7add9c89f7480ed0ed8dc57c2
UsenkoAnastasya <usenkonasty@gmail.com>
2026-03-26 16:20:00 +0200

    lfu створила

--- calorie-tracker/src/memoize.js
+++ calorie-tracker/src/memoize.js
@@ -15,14 +15,20 @@ export function calculateBMR(age, weight, height, gender, activity) {
   };
   return Math.round(bmr * activitylevel[activity]);
 }
-export function memoize(fn, { MAXSIZE = Infinity, strategy = "LRU" } = {}) {
+export function memoize(
+  fn,
+  { MAXSIZE = Infinity, strategy = "LRU", ttl = Infinity } = {},
+) {
   const map = new Map();
+  const freqMap = new Map();
+  const TimeMap = new Map();
   return function (...arr) {
     const key = JSON.stringify(arr);
     if (map.has(key)) {
       const value = map.get(key);
       map.delete(key);
       map.set(key, value);
+      freqMap.set(key, freqMap.get(key) + 1);
       return value;
     }
     const result = fn(...arr);
@@ -33,9 +39,20 @@ export function memoize(fn, { MAXSIZE = Infinity, strategy = "LRU" } = {}) {
         map.delete(keytodelete);
       } else if (strategy === "LRU") {
         map.delete(map.keys().next().value);
+      } else if (strategy === "LFU") {
+        const minFreq = Math.min(...freqMap.values());
+        for (const [key, freq] of freqMap) {
+          if (freq === minFreq) {
+            freqMap.delete(key);
+            map.delete(key);
+            break;
+          }
+        }
+      } else if (strategy === " TIME") {
       }
     }
     map.set(key, result);
+    freqMap.set(key, 1);
     return result;
   };
 }
